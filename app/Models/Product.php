88157eaa555d8bd862762f87072669cd4b9d6a5d
JPabloPena <pablo27-99@hotmail.com>
2021-09-13 16:55:31 -0500

    Migrations and fakers

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,21 +11,27 @@ class Product extends Model
 {
     use HasFactory;
 
-    //attributes id, name, description, price, category, brand, warranty, created_at, updated_at
-    protected $fillable = ['id','name','description','price','category','brand','warranty'];
+    //attributes id, name, description, salePrice, loanPrice, category, brand, warranty, type, availableLoan, created_at, updated_at
+
+    //methods validateProduct
+    
+    protected $fillable = ['id','name','description','salePrice','loanPrice','category','brand','warranty','type','availableLoan'];
 
     public static function validateProduct(Request $request)
     {
         $request->validate([
             "name" => "required",
             "description" => "required",
-            "price" => "required|numeric|gt:0",
+            "salePrice" => "required|numeric|gt:0",
+            "loanPrice" => "required|numeric|gt:0",
             "category" => "required",
             "brand" => "required",
-            "warranty" => "required"
+            "warranty" => "required",
+            "type" => "required",
+            "availableLoan" => "required"
         ]);
 
-        Product::create($request->only(["name","description","price","category","brand","warranty"]));
+        Product::create($request->only(["name","description","salePrice","loanPrice","category","brand","warranty","type","availableLoan"]));
     }
 
     public function getId()
@@ -58,14 +64,24 @@ class Product extends Model
         $this->attributes['description'] = $description;
     }
 
-    public function getPrice()
+    public function getSalePrice()
     {
-        return $this->attributes['price'];
+        return $this->attributes['salePrice'];
     }
 
-    public function setPrice($price)
+    public function setSalePrice($salePrice)
     {
-        $this->attributes['price'] = $price;
+        $this->attributes['salePrice'] = $salePrice;
+    }
+
+    public function getLoanPrice()
+    {
+        return $this->attributes['loanPrice'];
+    }
+
+    public function setLoanPrice($loanPrice)
+    {
+        $this->attributes['loanPrice'] = $loanPrice;
     }
 
     public function getCategory()
@@ -96,5 +112,25 @@ class Product extends Model
     public function setWarranty($warranty)
     {
         $this->attributes['warranty'] = $warranty;
+    }
+
+    public function getType()
+    {
+        return $this->attributes['type'];
+    }
+
+    public function setType($type)
+    {
+        $this->attributes['type'] = $type;
+    }
+
+    public function getAvailableLoan()
+    {
+        return $this->attributes['availableLoan'];
+    }
+
+    public function setAvailableLoan($availableLoan)
+    {
+        $this->attributes['availableLoan'] = $availableLoan;
     }
 }
---
+++ database/migrations/2021_09_13_000000_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('description');
+            $table->float('salePrice');
+            $table->float('loanPrice');
+            $table->text('category');
+            $table->text('brand');
+            $table->text('warranty'); //Podría ser date
+            $table->text('type');
+            $table->boolean('availableLoan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
