e961548f2ebbd15a34ee818951d5e6912e53c44a
fsociety-fenix <fsociety.fenix@gmail.com>
2021-11-15 22:48:54 -0500

    added language switch to footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,8 +128,15 @@
         @yield('content')
     </div>
     <!-- Copyright -->
-    <div class="bg-secondary text-light text-center py-3">© 2021 Copyright:
-        <a href="{{route('home.index')}}"> CarPart.com</a>
+    <div class="container-fluid bg-secondary text-light text-center py-3">
+        <div class="row">
+            <div class="col">
+                <div class="">© 2021 Copyright:
+                    <a href="{{route('home.index')}}"> CarPart.com</a>
+                </div>
+            </div>
+            <div class="col"><a href="/language/en">English</a> | <a href="/language/es">Español</a></div>
+        </div>
     </div>
     <!-- Copyright -->
     <!-- Bootstrap core JS-->
